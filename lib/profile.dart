04580c9d38e135df7355d1ad00e3c917a96535b0
tionx3na <1runx3na2@gmail.com>
2021-07-14 00:01:35 +0530

    UI Part-2-Profile

--- lib/profile.dart
+++ lib/profile.dart
@@ -5,8 +5,36 @@ class Profile extends StatelessWidget {
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(child: Text('Profile Page')),
       drawer: MyDrawer(),
+      body: Container(
+        color: Colors.grey.shade900,
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height/3,
+              width: MediaQuery.of(context).size.width,
+              decoration: new BoxDecoration(
+                color: Colors.grey.shade900,
+                gradient: new LinearGradient(
+                  colors: [Colors.grey, Colors.grey.shade900],
+                  ),
+              ),
+              child: Container(
+                margin: EdgeInsets.all(10.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  shape: BoxShape.circle,
+                  image: new DecorationImage(image: AssetImage('images/propic.jpeg')),
+                  ),
+                ),
+              ),
+
+            Container(
+              
+            )
+          ],
+        ),
+      ),
     );
   }
 }
