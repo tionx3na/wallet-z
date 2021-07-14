c0bc7489c3f0cf9a24128ba8775aa2052853c239
tionx3na <1runx3na2@gmail.com>
2021-07-14 20:07:01 +0530

    UI-Part-2-Profile-top-section

--- lib/profile.dart
+++ lib/profile.dart
@@ -9,30 +9,37 @@ class Profile extends StatelessWidget {
       body: Container(
         color: Colors.grey.shade900,
         child: Stack(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height/3,
-              width: MediaQuery.of(context).size.width,
-              decoration: new BoxDecoration(
-                color: Colors.grey.shade900,
-                gradient: new LinearGradient(
-                  colors: [Colors.grey, Colors.grey.shade900],
-                  ),
-              ),
+          children: <Widget> [
+            Positioned(
+              top: 0.0,
               child: Container(
-                margin: EdgeInsets.all(10.0),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.circle,
-                  image: new DecorationImage(image: AssetImage('images/propic.jpeg')),
-                  ),
+                height: MediaQuery.of(context).size.height/3,
+                width: MediaQuery.of(context).size.width,
+                decoration: new BoxDecoration(
+                  color: Colors.grey.shade900,
+                  gradient: new LinearGradient(
+                    colors: [Colors.black, Colors.grey.shade600],
+                    ),
                 ),
-              ),
-
-            Container(
-              
-            )
-          ],
+                ),
+            ),
+          Positioned(
+              left: 0,
+              right: 0,  
+              top: 0,
+              bottom: 250,
+              child: Container(
+                
+                margin: EdgeInsets.all(50.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                    image: new DecorationImage(image: AssetImage('assets/images/propic.jpeg'), fit: BoxFit.contain),
+                    ),
+                
+              )
+           )
+            ],
         ),
       ),
     );
