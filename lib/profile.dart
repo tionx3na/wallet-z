d9f394511189e88d2e136ebed7d9a487c3453575
tionx3na <1runx3na2@gmail.com>
2021-07-15 00:13:08 +0530

    UI-PART-2-profile-page-2

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'main.dart';
+import 'currencies.dart';
 
 class Profile extends StatelessWidget {
   @override 
@@ -23,11 +24,12 @@ class Profile extends StatelessWidget {
                 ),
                 ),
             ),
+
           Positioned(
               left: 0,
               right: 0,  
               top: 0,
-              bottom: 250,
+              bottom: 300,
               child: Container(
                 
                 margin: EdgeInsets.all(50.0),
@@ -35,13 +37,106 @@ class Profile extends StatelessWidget {
                     color: Colors.white,
                     shape: BoxShape.circle,
                     image: new DecorationImage(image: AssetImage('assets/images/propic.jpeg'), fit: BoxFit.contain),
-                    ),
-                
+                    ),  
               )
-           )
-            ],
+           ),
+
+          Positioned(        
+            bottom: 0,
+            child: Container(
+              height: MediaQuery.of(context).size.height/1.1,
+              width: MediaQuery.of(context).size.width,
+              child: Center( 
+               child: Text("ARUN BABU", style: TextStyle(color: Colors.grey.shade600, fontSize: 50,),),
+              )
+            ),
+          ),
+         
+         DraggableScrollableSheet(
+        initialChildSize: 0.4,
+        minChildSize: 0.4,
+        maxChildSize: 0.6,
+        builder: (BuildContext context, myscrollController) {
+          return Container(
+             color: Colors.black38,
+             child: Container(
+               height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: ListView(      
+                  controller: myscrollController,
+                  children: <Widget>[
+                    userPortfolio(Icons.verified_user, "NAME:", "Arun Babu",),
+                    userPortfolio(Icons.verified_user, "USERNAME:", "@1runx3na",),
+                    userPortfolio(Icons.verified_user, "TOTAL:", r"$ 400",),
+                    userPortfolio(Icons.verified_user, "SIGN IN:", r"30/04/2021",),
+                   
+                  ],
+                )
+             ),
+          );
+        },
+         )
+         
+         ],
+          ),
         ),
-      ),
     );
   }
 }
+
+userPortfolio(IconData icon, String divname, String div) =>
+        Card(
+          elevation: 1.0,
+          child: InkWell(
+            onTap: () => print("tapped"),
+            child: Container(
+              height: 100.0,
+              padding: EdgeInsets.only(top: 15.0, bottom: 15.0, right: 15.0),
+              decoration: BoxDecoration(
+                  color: Colors.black,
+                 ),
+              child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(left: 5.0, right: 10.0),
+                              child: Icon(icon, color: Colors.green),
+                            ),
+
+                            Padding(
+                             padding: EdgeInsets.only(left: 5.0, right: 5.0), 
+                             child: Text(
+                                divname,
+                                style: TextStyle(
+                                    fontSize: 18.0, fontWeight: FontWeight.bold, color: Colors.orange),
+                              ),
+                            ),
+                            
+                            Padding(
+                              padding: EdgeInsets.only(left: 5.0, right: 5.0), 
+                              child: Text(div,
+                              
+                                  style: TextStyle(
+                                      fontSize: 16.0,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white))
+                            )
+                          ],
+                        ),
+
+                        Divider(height: 10.0,)
+                       
+                      ],
+             
+                    ),
+            ),
+          ),
+        );
+
+
+
+
+
+
