29aef9d407995cbd7bfafd60995d931bb4c2f494
tionx3na <1runx3na2@gmail.com>
2021-07-18 22:44:12 +0530

    UI-Part4-News-star

--- lib/news.dart
+++ lib/news.dart
@@ -2,11 +2,36 @@ import 'package:flutter/material.dart';
 import 'main.dart';
 
 class News extends StatelessWidget {
+  final GlobalKey<ScaffoldState> _scaffoldKey1 = new GlobalKey<ScaffoldState>();
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(child: Text('News Page')),
+      key: _scaffoldKey1,
       drawer: MyDrawer(),
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 0.0,
+            bottom: MediaQuery.of(context).size.height/1.15,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              color: Colors.grey.shade900,
+              height: MediaQuery.of(context).size.height/7,
+              child: Row(
+                children: <Widget> [
+                  IconButton(onPressed: () { _scaffoldKey1.currentState?.openDrawer();}, icon: Icon(Icons.menu), color: Colors.white, ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 90.0, right: 5.0),
+                    child: Text("WALLET-Z", style: TextStyle(color: Colors.grey, fontSize: 20.0, fontWeight: FontWeight.bold,)),
+                  )
+                ]
+              )
+            )
+          ),
+        ],
+      )
     );
   }
 }
