cd81fb326f50803255e40479e90f8c022b782d44
tionx3na <1runx3na2@gmail.com>
2021-07-19 00:08:48 +0530

    UI-Part5-About_us_start

--- lib/about.dart
+++ lib/about.dart
@@ -2,11 +2,56 @@ import 'package:flutter/material.dart';
 import 'main.dart';
 
 class AboutPage extends StatelessWidget {
+ final GlobalKey<ScaffoldState> _scaffoldKey1 = new GlobalKey<ScaffoldState>();
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(child: Text('About Page')),
+      key: _scaffoldKey1,
       drawer: MyDrawer(),
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 0.0,
+            bottom: MediaQuery.of(context).size.height/1.15,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              color: Colors.grey.shade900,
+              height: MediaQuery.of(context).size.height/7,
+              child: Row(
+                children: <Widget> [
+                  IconButton(onPressed: () { _scaffoldKey1.currentState?.openDrawer();}, icon: Icon(Icons.menu), color: Colors.white, ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 90.0, right: 5.0),
+                    child: Text("WALLET-Z", style: TextStyle(color: Colors.grey, fontSize: 20.0, fontWeight: FontWeight.bold,)),
+                  )
+                ]
+              )
+            )
+          ),
+
+          Positioned(
+             top: MediaQuery.of(context).size.height/7.5,
+            bottom: MediaQuery.of(context).size.height/2,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              color: Colors.cyan,)
+          ),
+
+          Positioned(
+            top: MediaQuery.of(context).size.height/2.2,
+            bottom: 0.0,
+            left: 0.0,
+            right: 0.0,
+            child: Container(
+              color: Colors.transparent,
+              child: Text("HELLO", style: TextStyle(fontSize: 20.0, color: Colors.white)),
+              )
+          )
+        ],
+      ),
     );
   }
 }
